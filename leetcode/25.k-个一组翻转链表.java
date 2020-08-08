7f30ef262a298eab8219b22f4eaafa7b19c4ec5c
shuangshuangshuangfeng <775001644@qq.com>
2020-08-08 17:15:28 +0800

    '2020-8-8'

--- leetcode/25.k-个一组翻转链表.java
+++ leetcode/25.k-个一组翻转链表.java
@@ -20,12 +20,67 @@ import sun.security.util.HexDumpEncoder;
 class Solution {
 
     /**
-     * 递归的方法
+     * 尾插法
      * @param head
      * @param k
      * @return
      */
     public ListNode reverseKGroup(ListNode head, int k) {
+        // 反转后链表的头指针
+        ListNode result = new ListNode();
+        result.next = head;
+        // 指向待翻转部分的头指针
+        ListNode reverse_head = result;
+        // 指向待翻转部分的尾指针
+        ListNode reverse_tail = result;
+        
+        while(true){
+            // 计数器
+            int cnt = k;
+            // 找到待翻转尾部
+            while(cnt>0 && reverse_tail!=null){
+                cnt--;
+                reverse_tail = reverse_tail.next;
+            }
+
+            if(reverse_tail == null) break;
+            //暂存新head
+            ListNode new_head = reverse_head.next;
+            
+            // 尾插法翻转
+            while(reverse_head.next != reverse_tail){
+                ListNode temp = reverse_head.next;
+                reverse_head.next = temp.next;
+                temp.next = reverse_tail.next;
+                reverse_tail.next = temp;
+                
+            }
+            // 重置新的待翻转起点
+            reverse_head = new_head;
+            reverse_tail = new_head;
+            // System.out.println("new head:"+new_head.val);
+
+            // print
+            // ListNode r = result;
+            // while(r != null){
+            //     System.out.print(r.val+"-");
+            //     r = r.next;
+            // }
+            // System.out.println();
+            
+        }
+    
+        return result.next;
+    }
+
+
+    /**
+     * 递归的方法
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode reverseKGroup_digui(ListNode head, int k) {
         // 链表分为三部分：
         // 1. 已反转， 2，待反转， 3.未反转
         if(head == null || head.next == null) return head;
