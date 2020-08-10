bb50f175b8c8a1b50824667c5a3ae4da6285729f
shuangshuangshuangfeng <775001644@qq.com>
2020-08-10 14:56:18 +0800

    'a'

--- leetcode/84.柱状图中最大的矩形.java
+++ leetcode/84.柱状图中最大的矩形.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /*
  * @lc app=leetcode.cn id=84 lang=java
  *
@@ -7,13 +9,38 @@
 // @lc code=start
 class Solution {
    
-   // 1. 暴力解法
+    // 3. 空间换时间, 单调栈
+    // 时间复杂度为O(N)
+    // 空间复杂度为O(N)
+    public int largestRectangleArea(int[] heights) {
+        // 用来存储最小柱子高度的index
+        Stack<Integer> stack = new Stack<>();
+
+        // copy一份新的数组
+        int[] tmp = new int[heights.length + 2];
+        System.arraycopy(heights, 0, tmp, 1, heights.length);
+
+        int max_area = 0; //最大面积
+
+        for(int i=0; i<tmp.length; i++){  
+            while(!stack.isEmpty() && tmp[i]<tmp[stack.peek()]){
+                // 如果当前tmp[i]小于tmp[stack.peek()]
+                int index = stack.pop();
+                int height = tmp[index];
+                max_area = Math.max(max_area, (i-stack.peek()-1)*height);
+            }
+            stack.push(i);
+        }
+        return max_area;
+    }
+
+    // 2. 暴力解法2
     // 时间复杂度为O(N^3)
     // 空间复杂度为O(1)
-    public int largestRectangleArea(int[] heights) {
+    public int largestRectangleArea_2(int[] heights) {
         int max_area = 0; //最大面积
-        for(int i=0; i<heights.length; i++){  // 右边界
-            for(int j=0; j<=i; j++){ //左边界
+        for(int i=0; i<heights.length; i++){  
+            for(int j=0; j<=i; j++){ 
                 // 左右边界中间最小高度
                 int min_height = heights[j];
                 for(int m=j; m<=i; m++){
@@ -29,7 +56,8 @@ class Solution {
     }
 
 
-    // 1. 暴力解法
+
+    // 1. 暴力解法1
     // 时间复杂度为O(N^3)
     // 空间复杂度为O(1)
     public int largestRectangleArea_1(int[] heights) {
@@ -49,6 +77,7 @@ class Solution {
         }
         return max_area;
     }
+
 
 }
 // @lc code=end
